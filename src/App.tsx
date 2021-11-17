9d03865cb9385111de2f45458d645781827ba4c3
Mohamed Elbaz <albaz2000eg@gmail.com>
2021-11-17 14:17:23 +0200

    fetch data from api depending on user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,112 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import 'tailwindcss/tailwind.css';
+import ErrorMessage from './components/ErrorMessage';
+import Loading from './components/Loading';
+import { API_URL, COLUMN } from './constants';
+import useFetch from './hooks/useFetch';
+import { Column, ColumnData, FetchOptions } from './types';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [dimensionColumn, setDimensionColumn] = useState<Column | null>(null);
+  const [measureColumns, setMeasureColumns] = useState<Column[]>([]);
+  const options: FetchOptions = useMemo(
+    () => ({
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // Example request body:
+      // {
+      //   "measures": ["Cost"],
+      //   "dimension": "Product"
+      //  }
+      body: JSON.stringify({
+        measures: measureColumns?.map(col => col.name),
+        dimension: dimensionColumn?.name
+      })
+    }),
+    [dimensionColumn, measureColumns]
+  );
+
+  const {
+    data: columnsData,
+    loading: columnsLoading,
+    error: columnsError
+  } = useFetch(`${API_URL}/columns`);
+
+  const { data, loading, error } = useFetch(
+    `${API_URL}/data`,
+    options,
+    // Skip executing the post request if the user hadn't selected any columns yet
+    !dimensionColumn || !measureColumns?.length
+  );
+
+  console.log({ data, loading, error });
+  console.log(columnsData, columnsLoading, columnsError);
+
+  const updateSelectedColumns = (column: Column) => {
+    if (column.function === COLUMN.DIMENSION) {
+      // Selected dimension column has to be only one
+      setDimensionColumn(column);
+    } else if (column.function === COLUMN.MEASURE) {
+      // Selected measure columns can be more than one
+      // Remove the measure column if it exists, or add it to the array if it doesn't
+      const hasColumn = measureColumns.find(col => col.name === column.name);
+      if (!hasColumn) {
+        // Push the clicked column
+        setMeasureColumns([...measureColumns, column]);
+      } else {
+        // Return all but the clicked column
+        setMeasureColumns(
+          measureColumns.filter(col => col.name !== column.name)
+        );
+      }
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex">
+      <div className="flex flex-col">
+        {columnsError ? (
+          <ErrorMessage error={columnsError} />
+        ) : columnsLoading ? (
+          <Loading />
+        ) : (
+          columnsData?.map(column => (
+            <button
+              key={column.name}
+              onClick={() => updateSelectedColumns(column as Column)}
+            >
+              {column.name}
+            </button>
+          ))
+        )}
+      </div>
+      <div className="flex flex-col">
+        <div>Dimension column {dimensionColumn?.name}</div>
+        <div>
+          Measure columns{' '}
+          {measureColumns?.map(col => (
+            <span key={col.name}>{col.name}</span>
+          ))}
+        </div>
+        <div className="max-w-lg p-4">
+          {error ? (
+            <ErrorMessage error={error} />
+          ) : loading ? (
+            <Loading />
+          ) : data?.length ? (
+            data.map(col => (
+              <div key={col.name}>
+                <p className="font-bold underline">{col.name}</p>
+                <p>{(col as ColumnData).values.join('-')}</p>
+              </div>
+            ))
+          ) : (
+            <p>Please select columns</p>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
