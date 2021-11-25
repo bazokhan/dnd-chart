f793044f19a5be429f79b20b1e5d78f82a3d6036
Mohamed Elbaz <albaz2000eg@gmail.com>
2021-11-25 17:53:43 +0200

    add mock fetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,6 +3,70 @@ import { FetchOptions, FetchResponse } from '../types';
 
 const defaultOptions: FetchOptions = { method: 'GET' };
 
+// mock fetch
+const fetch = (url: string, options: unknown): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    if (url?.endsWith('columns')) {
+      resolve({
+        json: () => {
+          return [
+            {
+              name: 'Product',
+              function: 'dimension'
+            },
+            {
+              name: 'Year',
+              function: 'dimension'
+            },
+            {
+              name: 'Country',
+              function: 'dimension'
+            },
+            {
+              name: 'Cost',
+              function: 'measure'
+            },
+            {
+              name: 'Revenue',
+              function: 'measure'
+            },
+            {
+              name: 'Units sold',
+              function: 'measure'
+            }
+          ];
+        }
+      });
+    } else if (url?.endsWith('data') && options) {
+      resolve({
+        json: () => {
+          return [
+            {
+              name: 'Product',
+              values: [
+                'Diskette',
+                'Memory Card',
+                'HDTV Tuner',
+                'Flat Panel Graphics Monitor',
+                'Digital Camera',
+                'Minitower Speaker',
+                'Extension Cable',
+                'Y Box'
+              ]
+            },
+            {
+              name: 'Cost',
+              values: [333.08, 7.07, 10.77, 194.76, 13.18, 143.3, 20.2, 405]
+            }
+          ];
+        }
+      });
+    } else {
+      reject(new Error('Something went wrong'));
+    }
+  });
+};
+
 const useFetch = (
   url: string,
   options: FetchOptions = defaultOptions,
